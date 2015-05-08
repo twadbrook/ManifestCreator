8463d6f8fcc84de04e3d334c5bb0fc88ec2306ed
twadbrook <twadbrook@milkenschool.org>
2015-05-08 11:49:16 -0700

    Fixed to use values

--- ManifestCreator.py
+++ ManifestCreator.py
@@ -5,11 +5,11 @@ import os
 
 MUNKI_REPO = "/Users/Shared/munki/manifests"
 
-with open('serials.csv', mode='r') as infile:
+with open('/Library/Server/Web/Data/Sites/Default/serials.csv', mode='r') as infile:
     reader = csv.reader(infile)
     keyDict = dict(reader)
 
-for key in keyDict.keys():
-#	f = open(os.path.join(MUNKI_REPO, str(key)), 'w')
-#	f.close()
-	print key
+for items in keyDict.values():
+	f = open(os.path.join(MUNKI_REPO, str(items)), 'w')
+	f.close()
+#	print items
